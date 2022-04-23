324bd443f42a4b2e8801e062521807720d58b34d
Ali Mousavi <amousavig@icloud.com>
2022-04-23 04:46:57 +0430

    Finished working on views.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -28,3 +28,27 @@ $('#dark-mode-btn').click(function() {
     }
 });
 // END DARK MODE
+// MODAL
+function showModal(modal) {
+    modal.addClass('active');
+    $('body').addClass('modal-active');
+}
+function hideModal(modal) {
+    modal.removeClass('active');
+    $('body').removeClass('modal-active');
+}
+$(document).on('click', '[data-modal]', function() {
+    var modalId = $(this).attr('data-modal');
+    showModal($('#' + modalId));
+});
+$(document).on('click', '.modal-close', function() {
+    hideModal($(this).closest('.modal'));
+});
+// HIDE MODAL ON CLICK OUTSIDE
+$(document).on('click', '.modal', function(e) {
+    if (e.target !== this) {
+        return;
+    }
+    hideModal($(this));
+});
+// END MODAL
